044420919289da31dac6d30074ef779b1c909611
Mikhail Krasikov <corolish@gmail.com>
2026-04-15 23:09:13 +0300

    Release v0.1.9

--- backend/src/main/java/com/mkrasikoff/epigraph/model/Quote.java
+++ backend/src/main/java/com/mkrasikoff/epigraph/model/Quote.java
@@ -1,10 +1,13 @@
 package com.mkrasikoff.epigraph.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -12,11 +15,17 @@ import lombok.Data;
 @Data
 public class Quote {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Quote text must not be empty")
+    @Size(max = 3000, message = "Quote text must not exceed 3000 characters")
+    @Column(length = 3000)
     private String text;
 
+    @Size(max = 255, message = "Author name must not exceed 255 characters")
+    @Column(length = 255)
     private String author;
 
     private String source;
@@ -31,5 +40,6 @@ public class Quote {
     /**
      * Unix timestamp в миллисекундах
      */
+    @Column(updatable = false)
     private Long added;
 }
