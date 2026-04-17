72bb0ced85a096b3c90ad1d5b4e7ca9bb244bfe0
Mikhail Krasikov <corolish@gmail.com>
2026-04-17 23:48:06 +0300

    [TASK-31] Liquibase integration fix

--- backend/src/main/resources/db/postgres/001-initial-schema.sql
+++ backend/src/main/resources/db/postgres/001-initial-schema.sql
@@ -1,11 +1,13 @@
 --liquibase formatted sql
 --changeset mkrasikoff:001-initial-schema
-CREATE TABLE IF NOT EXISTS quotes (
-                                      id      BIGSERIAL PRIMARY KEY,
-                                      added   BIGINT,
-                                      author  VARCHAR(255),
-    fav     BOOLEAN NOT NULL DEFAULT FALSE,
-    source  VARCHAR(255),
-    tags    VARCHAR(255),
-    text    VARCHAR(3000) NOT NULL
-    );
+
+CREATE TABLE IF NOT EXISTS quotes
+(
+    id     BIGSERIAL    PRIMARY KEY,
+    added  BIGINT,
+    author VARCHAR(255),
+    fav    BOOLEAN      NOT NULL DEFAULT FALSE,
+    source VARCHAR(255),
+    tags   VARCHAR(255),
+    text   VARCHAR(3000) NOT NULL
+);
--- backend/src/main/resources/db/postgres/002-extend-source-tags-columns.sql
+++ backend/src/main/resources/db/postgres/002-extend-source-tags-columns.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 --changeset mkrasikoff:002-extend-source-tags-columns
-ALTER TABLE quotes
-ALTER COLUMN source TYPE VARCHAR(500),
-    ALTER COLUMN tags   TYPE VARCHAR(500);
+
+ALTER TABLE 
+  quotes ALTER COLUMN source TYPE VARCHAR(500), 
+  ALTER COLUMN tags TYPE VARCHAR(500);
